8772f46fdba35427c29d10b517cd47f908d7d54b
Paulo Souza <paulovictorfds@ufrn.edu.br>
2021-01-23 16:21:33 -0300

    commit

--- questao13/resposta.c
+++ questao13/resposta.c
@@ -26,9 +26,10 @@ int main(void)
         scanf("%f", &array[i]);
     }
 
+    // Chama função de ordenação do array
     bubble_sort(array, N);
 
-    // Imprime os valores do arry ordenado
+    // Imprime os valores do array ordenado
     printf("Os valores digitados em ordem crescente sao:\n");
     for (unsigned int i = 0; i < N; i++)
     {
@@ -38,25 +39,25 @@ int main(void)
     free(array);
 }
 
-// Função de ordenação do vetor
+// Função para ordenação do array
 void bubble_sort(float* array, unsigned int N)
 {
     int swap_counter = -1;
     float aux = 0.0;
 
     do
+    {
+        swap_counter = 0;
+        for (unsigned int i = 0; i < (N - 1); i++)
         {
-            swap_counter = 0;
-            for (unsigned int i = 0; i < (N - 1); i++)
+            if (array[i] > array[i + 1])
             {
-                if (array[i] > array[i + 1])
-                {
-                    aux = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = aux;
-                    swap_counter++;
-                }
+                aux = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = aux;
+                swap_counter++;
             }
+        }
     }
     while(swap_counter != 0);
 }
